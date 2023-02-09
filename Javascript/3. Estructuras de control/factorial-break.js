8b294d01b9428355d6cbd020d4a72fec5110b361
Anthony <admm1225@gmail.com>
2023-02-09 13:12:26 -0500

    Ejercicio 3

---
+++ Javascript/3. Estructuras de control/factorial-break.js
@@ -0,0 +1,13 @@
+let i = 1
+let resultado = 1
+
+while (true) {
+    resultado *= i;
+    i++
+
+    if (i===11) {
+        break;
+    }
+}
+
+console.log(resultado);
---
+++ Javascript/3. Estructuras de control/factorial-for.js
@@ -0,0 +1,7 @@
+let resultado = 1;
+
+for (let i = 1; i <= 10; i++) {
+    resultado = i * resultado;
+}
+
+console.log(resultado);
