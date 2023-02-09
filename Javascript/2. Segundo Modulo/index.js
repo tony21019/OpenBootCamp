87aa0d178b0a71d6d42988a4cbbb37e24f52a460
Anthony <admm1225@gmail.com>
2023-02-08 23:19:11 -0500

    Ejercicio 2

---
+++ Javascript/2. Segundo Modulo/index.js
@@ -0,0 +1,12 @@
+const ar = [
+    "Anthony", 
+    20,
+    true,
+    fecha = new Date("December 25 2002"),
+    libro = {
+        titulo: "El marciano",
+        autor: "Andy Weir",
+        fecha: 2011,
+        url: "https://librerianacional.com/producto/el-marciano"
+    }
+]
