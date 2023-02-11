bac06d633feee20f3c2af09578ca0a82d627d24e
Anthony <admm1225@gmail.com>
2023-02-11 16:40:29 -0500

    Numeros

---
+++ Javascript/5. Numeros/index.js
@@ -0,0 +1,12 @@
+let alturaCm = 180;
+let alturaM = 1.80;
+let peso = 55.2;
+
+let alturaRedon = alturaM.toFixed(1);
+let pesoRedon = Math.floor(peso);
+
+let valor;
+
+if ((Number.MAX_VALUE + 1) === Number.MAX_VALUE) {
+    valor = true;
+}
