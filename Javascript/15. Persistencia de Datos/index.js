78e70f2f440ed8587628b95cdf622e56e8deeeae
Anthony <admm1225@gmail.com>
2023-03-06 14:03:45 -0500

    Cookies

---
+++ Javascript/15. Persistencia de Datos/index.js
@@ -0,0 +1,13 @@
+const nombre = "Anthony"
+const apellido = "Molina"
+
+const datos = {
+    nombre,
+    apellido
+}
+
+sessionStorage.setItem("datos", JSON.stringify(datos))
+localStorage.setItem("datos", JSON.stringify(datos))
+
+const now = new Date()
+document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
